eb25d6da9f06234cc13b2687dcb91deb7029b9e6
kdjelle <kylejelle@gmail.com>
2018-05-21 18:52:39 -0400

    Kyle's lab_input

---
+++ lab_input/input_ip.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#Written by Kyle Jelle (sort of)
+#Collect IP address from user and display it.
+
+#Collect user input
+user_input = input('Please enter an IPv4 IP address:')
+
+#Print input collected from user
+print("You told me the IPv4 address is:" + user_input)
+
+#The first line should collect and store input from the user. Ask the user for the ‘vendor name’ associated with the device. Use any variable name you would like.
+#Use a second line of code to print the input you just collected from the user.
+
