21292d77c55dfeb6f5caff2df801a74f9bfdab8b
kdjelle <kylejelle@gmail.com>
2018-05-22 16:56:13 -0400

    Update Orange

--- day2/ipask.py
+++ day2/ipask.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #Written by Kyle Jelle
 #Collecting IP and MAC addresses
-IP = str(input('What is your IP address? '))
+IP=str(input('What is your IP address? '))
 MAC=str(input('What is your MAC address? '))
 print('')
 print('You claimed your IP address was',IP,'and your MAC address was',MAC)
