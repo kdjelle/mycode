86ae91bb66b523bd4428aecfb618e770cbd451b4
kdjelle <kylejelle@gmail.com>
2018-05-22 11:23:16 -0400

    Orange

---
+++ day2/orange.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+#Written by Kyle Jelle
+#Printing ORANGE using six(6) print() functions to display the word.
+print ('O', end='')
+print ('R', end='')
+print ('A', end='')
+print ('N', end='')
+print ('G', end='')
+print ('E')
