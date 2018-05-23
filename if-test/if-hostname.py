562991b11fc6c48208f6caa2b565944b4cc7ea5b
kdjelle <kylejelle@gmail.com>
2018-05-23 01:03:46 -0400

    Lab 11

---
+++ if-test/if-hostname.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# By Kyle Jelle
+# This is lab11
+
+#7. Creating a string we can test against
+hostname = 'MTG'
+
+# Test logic!
+if hostname == 'MTG':
+    print('The hostname was found to be mtg')
