9d07a5ba8e1118018688103c1c1e7907fbec45e4
kdjelle <kylejelle@gmail.com>
2018-06-04 03:01:57 -0400

    Lab 60 complete

---
+++ day09-weekend/lab60/myconfig.py
@@ -0,0 +1,14 @@
+# import code from NAPALM
+from napalm import get_network_driver
+    
+# Tell NAPALM to speak "eos" commands to our switches (very cisco-like)
+driver = get_network_driver('eos')
+    
+# Hard-code the switch credentials
+device = driver('172.16.2.10', 'admin', 'alta3') 
+    
+#Equates to: ssh into the switch, login, and enable
+device.open() 
+    
+# Print STARTUP, RUNNING, and CANDIDATE configs 
+print(device.get_config())
