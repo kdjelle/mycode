f310330fa1ff429936bbf22a946d90461aac6e6f
kdjelle <kylejelle@gmail.com>
2018-06-06 13:22:13 -0400

    Lab 70 complete

---
+++ day12/sort02/sorted01.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+iplist = ['10.8.2.1', '1.1.1.1', '255.255.255.255', '10.1.2.1', '10.2.1.2', '10.2.3.2', '10.7.2.1', '192.168.23.1', '192.168.66.1', '10.42.2.1', '10.11.10.2', '10.25.32.2', '10.27.21.1', '192.168.55.1']
+print('Currently iplist looks like this:', iplist)
+print('\nThe results of sorted(iplist) function:', sorted(iplist))
+print('\nBut iplist has not actually changed:', iplist)
+
